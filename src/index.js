4ebbb544744c6ea8ad2a47f8c15d0300526d4fd2
Chris Cheung <c33cheun@gmail.com>
2017-08-29 14:06:45 -0400

    added footer, finish desktop style

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import App from './App';
 import Header from './js/Header.js';
 import Banner from './js/Banner.js';
 import CommunityMembersWrapper from './js/CommunityMembersWrapper.js';
+import Footer from './js/Footer.js';
 
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<Banner bannerStyle="community-members-banner"/>, document.getElementById('banner'));
 ReactDOM.render(<CommunityMembersWrapper />, document.getElementById('content'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
---
+++ src/js/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer h5 pv2 w-100 tc">
+        <div className="center">
+          <div className="w-20 pl3 fl"><h5 className="light-grey">© 2016</h5></div>
+          <div className="w-20 pr3 mv2 fr"><div className="pa3 footer-logo"></div></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
