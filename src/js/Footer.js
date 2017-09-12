18966546044aabb3dbd3b5155f7df96c9f40a3e0
Chris Cheung <c33cheun@gmail.com>
2017-09-12 10:07:30 -0400

    Fix up footer

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -5,8 +5,6 @@ class Footer extends Component {
     return (
       <div className="footer h5 pv2 w-100 tc">
         <div className="center">
-          <div className="w-20 pl3 fl"><h5 className="light-grey">© 2016</h5></div>
-          <div className="w-20 pr3 mv2 fr"><div className="pa3 footer-logo"></div></div>
         </div>
       </div>
     );
